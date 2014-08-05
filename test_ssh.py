a2d6eee4d2bedf9a6f7038c5b6aeb31bc2212dc1
Justin Azoff <justin@bouncybouncy.net>
2014-08-05 19:18:25 -0400

    test program

---
+++ test_ssh.py
@@ -0,0 +1,44 @@
+import ssh_runner
+
+import time
+time.sleep(2)
+
+def go(m):
+    print 'uptime is', m.exec_command("arpy", ["uptime"])
+
+    print "Single host:"
+
+    for res in m.exec_commands("arpy", (
+        ["uptime"],
+        ["uname", "-a"],
+        ["df", "-h", "/tmp"])):
+        print res
+
+    while True:
+        print
+        print "Serially:"
+        print 'arpy', m.exec_command("arpy", ["uname", "-a"])
+        print 'rp2', m.exec_command("rp2", ["uname", "-a"])
+        print
+        print "Parallel:"
+
+        for res in m.exec_multihost_commands([
+            ("arpy", ["uname", "-a"]),
+            ("rp2", ["uname", "-a"]),
+            ]):
+            print res
+
+        
+        print
+        print "Host status:"
+
+        for x in m.host_status():
+            print x
+
+        time.sleep(5)
+
+m = ssh_runner.MultiMasterManager()
+try :
+    go(m)
+finally:
+    m.shutdown()
